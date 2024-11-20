abdc6c370029ee2c090059f9826e1f299e1246fb
ann-nagovitsyna <ann.nagovitsyna629232@gmail.com>
2024-11-20 16:27:01 +0300

    Update Найти сумму произведений из списка словарей.py

--- Лабораторная работа 4/Найти сумму произведений из списка словарей.py
+++ Лабораторная работа 4/Найти сумму произведений из списка словарей.py
@@ -1,20 +1,33 @@
-import csv
 import json
 
-INPUT_FILENAME = "input.csv"
-OUTPUT_FILENAME = "output.json"
+def calculate_sum_of_products_from_file(json_file_path):
+    try:
+        with open(json_file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
 
-def task() -> None:
-    with open(INPUT_FILENAME, mode='r', encoding='utf-8') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=',')
-        data = [row for row in csv_reader]
+        total = 0.0
+        for item in data:
+            score = item.get('score', 0.0)
+            weight = item.get('weight', 0.0)
+            product = score * weight
+            total += product
 
-    with open(OUTPUT_FILENAME, mode='w', encoding='utf-8') as json_file:
-        json.dump(data, json_file, indent=4, ensure_ascii=False)
+        return round(total, 3)
+
+    except FileNotFoundError:
+        print(f"Файл '{json_file_path}' не найден.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Ошибка при чтении JSON файла '{json_file_path}'.")
+        return None
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        return None
 
 if __name__ == '__main__':
-    task()
+    json_file = 'input.json'
 
-    with open(OUTPUT_FILENAME, encoding='utf-8') as output_f:
-        for line in output_f:
-            print(line, end="")
+    total_sum = calculate_sum_of_products_from_file(json_file)
+
+    if total_sum is not None:
+        print(total_sum)
