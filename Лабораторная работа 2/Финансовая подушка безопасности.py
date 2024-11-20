794c53996257f121d2a53a1f5a194b7fa720e337
ann-nagovitsyna <ann.nagovitsyna629232@gmail.com>
2024-11-20 16:21:17 +0300

    Update Финансовая подушка безопасности.py

--- Лабораторная работа 2/Финансовая подушка безопасности.py
+++ Лабораторная работа 2/Финансовая подушка безопасности.py
@@ -1,18 +1,16 @@
-salary = 5000       # Ежемесячная зарплата
-spend = 6000        # Траты за первый месяц
-months = 10         # Количество месяцев, которое планируется протянуть без долгов
-increase = 0.03     # Ежемесячный рост цен
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
 
-money_capital = 0   # Необходимая подушка безопасности
+month = 0
 
-current_spend = spend
+while True:
+    budget = money_capital + salary
+    if spend > budget:
+        break
+    money_capital = budget - spend
+    spend *= (1 + increase)
+    month += 1
 
-for month in range(1, months + 1):
-    deficit = current_spend - salary
-    if deficit > 0:
-        money_capital += deficit
-    current_spend *= (1 + increase)
-
-money_capital = round(money_capital)
-
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
+print("Количество месяцев, которое можно протянуть без долгов:", month)
